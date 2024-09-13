783b69da9810ab93d02bc92b9fda5da04c775f65
YAO LIU <yliu0430@student.monash.edu>
2024-09-14 09:51:35 +1000

    init

---
+++ Beesly_training_Mac/Beesly_training_Mac/Beesly_training_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beesly_training_MacApp.swift
+//  Beesly_training_Mac
+//
+//  Created by Andy liu on 2024/9/14.
+//
+
+import SwiftUI
+
+@main
+struct Beesly_training_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
